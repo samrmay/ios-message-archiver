af6163069db58fc57448b9dc26a06b95160bb245
Sam May <samrmay@sas.upenn.edu>
2021-02-17 12:30:21 -0500

    add global vars, get_backup fcn

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import argparse
 import os
 import sqlite3
 import csv
+import time
+
+DATA_DICT = {'sms': ('Library/SMS/sms.db', 'message')}
+MESSAGE_DICT = {'is_from_me': 21, 'handle_id': 5,
+                'cache_roomnames': 35, 'text': 2}
+BOS_TOKEN = '<BOS>'
+EOS_TOKEN = '<EOS>'
 
 
 def parse_args():
@@ -20,9 +27,9 @@ def parse_args():
     return args.backup_path, args.contact_name, args.time_range, args.save_all
 
 
-class Message_archiver:
-    def __init__(self, backup_dir, contact_name, time_range, save_all):
-        self.backup_dir = backup_dir
+class MessageArchiver:
+    def __init__(self, backup_path, contact_name, time_range, save_all):
+        self.backup_path = backup_path
         if save_all:
             self.contact_name = '__all__'
         else:
@@ -33,10 +40,39 @@ class Message_archiver:
         else:
             self.time_range = '__all__'
 
+    def get_backup(self):
+        backups_arr = []
+        for backup in os.listdir(self.backup_path):
+            path = self.backup_path + backup
+            new_dict = {'path': path,
+                        'mtime': os.path.getmtime(path)}
+            backups_arr.append(new_dict)
+        if (len(backups_arr) == 0):
+            print('No backups found.')
+            return False
+        if (len(backups_arr) == 1):
+            print('One backup found.')
+            return backups_arr[0]
+
+        print('Select a backup from which to retrieve data:')
+        i = 0
+        for backup in backups_arr:
+            i += 1
+            print(f'{i}. {backup.get("path")}')
+            mtime = time.localtime(backup.get("mtime"))
+            print(f'    Last modified on {mtime[1]}/{mtime[2]}/{mtime[0]}')
+        target_index = int(input()) - 1
+        while len(backups_arr) < target_index < 0:
+            print(f'Error, pick an integer between 0-{len(backups_arr)}')
+            target_index = int(input()) - 1
+        return backups_arr[target_index]
+
 
 def main():
     backup_path, contact_name, time_range, save_all = parse_args()
-    print(backup_path, contact_name, time_range, save_all)
+    archiver = MessageArchiver(backup_path, contact_name, time_range, save_all)
+
+    archiver.get_backup()
 
 
 if __name__ == "__main__":
